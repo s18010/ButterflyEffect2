bf9f2e17c8bb9b0b0600cf76e37a54681d43a796
s18010 <s18010@std.it-college.ac.jp>
2019-11-08 15:15:44 +0900

    ホーム画面のデザイン修正

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
-import { ImageBackground, View, Text, StyleSheet, Button } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { useState, useContext } from 'react';
+import { Platform, ImageBackground, View, StyleSheet } from 'react-native';
+import { Button, Container, Icon, Text, Card } from 'native-base';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
+import { MainContext } from '../store/MainContext';
+import Colors from '../constants/Colors';
 
 
 const HomeScreen = (props) => {
-  const scannedData = useSelector(state => state.qr.qr.data)
-  const [selectedImage, setSelectedImage] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7EjvkRoy42l1dFnZNFb17qOmd8WGpuOiJn2xUjigouLoez_cTMQ&s");
+  const { currentPoints } = useContext(MainContext);
+
+  // const [selectedImage, setSelectedImage] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7EjvkRoy42l1dFnZNFb17qOmd8WGpuOiJn2xUjigouLoez_cTMQ&s");
+  const [selectedImage, setSelectedImage] = useState("https://m.media-amazon.com/images/I/61JljT2YNmL._SS500_.jpg");
 
   const moveToQRScreenHandler = () => {
     props.navigation.navigate('QRReader');
@@ -40,23 +44,38 @@ const HomeScreen = (props) => {
   };
 
   return (
-    <ImageBackground
-      source={{ uri: selectedImage }}
-      style={{ width: '100%', height: '100%' }}
-    >
-      <View style={styles.points}>
-        <Text style={styles.points}>{scannedData}Points</Text>
-      </View>
+    <Container>
+      <ImageBackground
+        source={{ uri: selectedImage }}
+        style={{ width: '100%', height: '100%' }}>
 
-      <Button
-        title="Scan QRcode"
-        onPress={moveToQRScreenHandler}
-      />
-      <Button
-        title="select Background Image"
-        onPress={takeImageHandler}
-      />
-    </ImageBackground >
+        <View style={styles.topContainer}>
+
+          <View style={styles.pointContainer}>
+            <Text style={styles.points}>{currentPoints}
+              <Text style={styles.pointText}>Points</Text>
+            </Text>
+          </View>
+
+          <Button info small
+            style={styles.qrButton}
+            onPress={moveToQRScreenHandler} >
+            <Text> QRコード読み取り </Text>
+          </Button>
+
+          {/* <Button
+            title="壁紙を変更"
+            onPress={takeImageHandler}
+          /> */}
+        </View>
+
+        <Button info
+          style={styles.changeWallpaperButton}
+          onPress={takeImageHandler} >
+          <Text>壁紙を変更</Text>
+        </Button>
+      </ImageBackground >
+    </Container>
   );
 };
 
@@ -66,6 +85,39 @@ HomeScreen.navigationOptions = () => {
   };
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  topContainer: {
+    position: "relative",
+    height: "20%",
+    width: "100%",
+    marginVertical: 20,
+    paddingVertical: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+  },
+  pointContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  points: {
+    fontSize: 50,
+    justifyContent: "center",
+  },
+  pointText: {
+    fontSize: 25,
+  },
+  qrButton: {
+    position: 'absolute',
+    right: 10,
+    bottom: 10,
+    display: 'flex',
+  },
+  changeWallpaperButton: {
+    position: "absolute",
+    bottom: 25,
+    right: 25,
+    color: Colors.primaryColor,
+    display: 'flex',
+  },
+});
 
 export default HomeScreen;
