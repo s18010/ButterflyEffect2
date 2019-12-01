8d6a373b1306fcfea0ae1299959f7f6186cdc4b5
s18010 <s18010@std.it-college.ac.jp>
2019-12-01 11:53:02 +0900

    APIキー間違えてあげてたの消した

--- contexts/firebase.js
+++ contexts/firebase.js
@@ -1,16 +1,7 @@
 import firebase from 'firebase'
 import 'firebase/firestore';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1pHK3CpFUhIo6V4j5VHjlbl2OK1A-HOY",
-  authDomain: "butterflyeffect-41403.firebaseapp.com",
-  databaseURL: "https://butterflyeffect-41403.firebaseio.com",
-  projectId: "butterflyeffect-41403",
-  storageBucket: "butterflyeffect-41403.appspot.com",
-  messagingSenderId: "725347367921",
-  appId: "1:725347367921:web:95b3cfdb400d61530ca250",
-  measurementId: "G-L0P49BX3ZY"
-};
+const firebaseConfig = "configKeys"
 
 firebase.initializeApp(firebaseConfig)
 
